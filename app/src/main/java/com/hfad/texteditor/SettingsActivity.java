de03bd0152ee9d6b7f5dfbb1d200d88e1fc0979d
Хороших Анастасия Сергеевна <20048@iat.iat>
2022-10-14 15:14:50 +0800

    Initial commit

---
+++ app/src/main/java/com/hfad/texteditor/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.hfad.texteditor;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+public class SettingsActivity extends PreferenceActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+// загружаем предпочтения из ресурсов
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
